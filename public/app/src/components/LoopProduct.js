5bf8c857d412548e5e8bb4806e71c875b644b1a5
pasdo501 <dominikpaschke@hotmail.com>
2019-04-17 23:02:09 +1200

    Products in loop visually identical to old site
    - All elements now present on products displayed in the loop
    - Quantity does update quantity internally, but add button doesn't do
    anything yet
    - Next step: add the ajax add to cart function
    - After that: sort & filter for the loop

--- public/app/src/components/LoopProduct.js
+++ public/app/src/components/LoopProduct.js
@@ -2,7 +2,32 @@ import React, { Fragment, Component } from "react";
 import { Link } from "react-router-dom";
 
 class LoopProduct extends Component {
+    state = {
+        quantity: "1",
+        quantityOptions: ["1"],
+    };
+
+    componentDidMount() {
+        const maxQuantity = 30;
+        const quantityOptions = [];
+        for (let i = 1; i <= maxQuantity; i++) {
+            quantityOptions.push(i.toString());
+        }
+
+        this.setState({
+            quantityOptions,
+        });
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            quantity: event.target.value
+        })
+    }
+
+
     render() {
+        const { quantity, quantityOptions } = this.state;
         const { meal } = this.props;
         const productBase = window.bbq_react_data.product_base;
 
@@ -33,8 +58,35 @@ class LoopProduct extends Component {
                     </Link>
                 </div>
                 <div className="uk-flex uk-flex-left uk-flex-middle cart">
-                    <div>Quantity</div>
-                    <div>Add</div>
+                    <div className="quantity">
+                        <label
+                            className="screen-reader-text"
+                            htmlFor={`quantity_${meal.id}`}
+                        >
+                            Quantity
+                        </label>
+                        <select
+                            id={`quantity_${meal.id}`}
+                            className="input-text qty text"
+                            name="quantity"
+                            title="Qty"
+                            aria-labelledby={meal.name}
+                            value={quantity}
+                            onChange={this.handleChange}
+                        >
+                            {quantityOptions.map((option) => (
+                                <option
+                                    key={option}
+                                    value={option}
+                                >
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <button className="button">
+                        Add
+                    </button>
                 </div>
             </Fragment>
         );
